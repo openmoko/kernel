e1ef0d2120b3cd908afe5ac40ba51cf62bb12231
mokopatches <mokopatches@openmoko.org>
2009-02-06 00:09:58 +0000

    input-nots-mousedev.patch This patch disables the reporting of touchscreen-like devices via /dev/input/mice.  In the Neo1973 (much like other handheld devices), we need this to distinguish between the touchscreen (which uses tslib) and optional additional usb/bluetooth mice that might be attached.
    Signed-off-by: Harald Welte <laforge@openmoko.org>

--- drivers/input/mousedev.c
+++ drivers/input/mousedev.c
@@ -1015,6 +1015,7 @@ static const struct input_device_id mousedev_ids[] = {
 		.evbit = { BIT_MASK(EV_KEY) | BIT_MASK(EV_REL) },
 		.relbit = { BIT_MASK(REL_WHEEL) },
 	},	/* A separate scrollwheel */
+#if 0
 	{
 		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
 				INPUT_DEVICE_ID_MATCH_KEYBIT |
@@ -1024,6 +1025,7 @@ static const struct input_device_id mousedev_ids[] = {
 		.absbit = { BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
 	},	/* A tablet like device, at least touch detection,
 		   two absolute axes */
+#endif
 	{
 		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
 				INPUT_DEVICE_ID_MATCH_KEYBIT |

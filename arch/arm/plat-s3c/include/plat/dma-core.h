37f5e035488e21244b9f27529486e5eef364c315
Ben Dooks <ben@simtec.co.uk>
2009-02-24 01:06:17 +0000

    S3C: Rename dma-core.h to dma-plat.h (update of previous patch)
    This is a patch to re-write the change of a previous part of the
    series where we rename dma.h to dma-core.h. This change should really
    be part of dma-plat.h for platform-specific bits of the dma and dma-core.h
    is for later addition.
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>

---
+++ arch/arm/plat-s3c/include/plat/dma-core.h
@@ -0,0 +1,10 @@
+/* arch/arm/plat-s3c/include/plat/dma.h
+ *
+ * <TOADD>
+ *
+ * Samsung S3C DMA core support
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/

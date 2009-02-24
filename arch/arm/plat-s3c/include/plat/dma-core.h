185a79d1cd888153dda9dfe1f9d266bfc4ca58df
Ben Dooks <ben@simtec.co.uk>
2009-02-24 01:06:28 +0000

    S3C: Move DMA channel management code to plat-s3c
    Change the name of S3C2410_DMA_CHANNELS to S3C_DMA_CHANNELS in the process.
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>

--- arch/arm/plat-s3c/include/plat/dma-core.h
+++ arch/arm/plat-s3c/include/plat/dma-core.h
@@ -1,6 +1,9 @@
 /* arch/arm/plat-s3c/include/plat/dma.h
  *
- * <TOADD>
+ * Copyright 2008 Openmoko, Inc.
+ * Copyright 2008 Simtec Electronics
+ *	Ben Dooks <ben@simtec.co.uk>
+ *	http://armlinux.simtec.co.uk/
  *
  * Samsung S3C DMA core support
  *
@@ -8,3 +11,12 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
 */
+
+extern struct s3c2410_dma_chan *s3c_dma_lookup_channel(unsigned int channel);
+
+extern struct s3c2410_dma_chan *s3c_dma_chan_map[];
+
+/* the currently allocated channel information */
+extern struct s3c2410_dma_chan s3c2410_chans[];
+
+

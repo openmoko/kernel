d9d2d2c3b1b599e182f910a8a7c9a163a9056ace
Thomas White <taw@bitwiz.org.uk>
2009-04-28 01:25:29 +0100

    Copyright notice
    This fixes the licence text at the top of glamo-cmdq.c, noting that code from
    Xorg has been incorporated.
    
    Signed-off-by: Thomas White <taw@bitwiz.org.uk>

--- drivers/mfd/glamo/glamo-cmdq.c
+++ drivers/mfd/glamo/glamo-cmdq.c
@@ -1,9 +1,8 @@
-/* Smedia Glamo 336x/337x command queue handling
+/*
+ * SMedia Glamo 336x/337x command queue handling
  *
  * Copyright (C) 2008-2009 Thomas White <taw@bitwiz.org.uk>
- * Based on xf86-video-glamo
- * Copyright  2007 OpenMoko, Inc.
- * Copyright © 2009 Lars-Peter Clausen <lars@metafoo.de>
+ * Based on xf86-video-glamo (see below for details)
  *
  * All rights reserved.
  *
@@ -21,7 +20,35 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
+ *
+ * Command queue handling functions based on those from xf86-video-glamo, to
+ * which the following licence applies:
+ *
+ * Copyright  2007 OpenMoko, Inc.
+ * Copyright © 2009 Lars-Peter Clausen <lars@metafoo.de>
+ *
+ * This driver is based on Xati,
+ * Copyright  2004 Eric Anholt
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
  */
+
 
 #include <drm/drmP.h>
 #include <drm/glamo_drm.h>

98b541c7129eb1b884433bf28d09527875081fc6
Nelson Castillo <arhuaco@freaks-unidos.net>
2009-10-24 14:25:45 -0500

    touchscreen patches for om-gta02-2.6.31
    On Sat, Oct 24, 2009 at 3:04 AM, Nelson Castillo
    <arhuaco@freaks-unidos.net> wrote:
    > Hi there. I finally made this work. I tested with: cat /dev/input/touchscreen0 | hexdump
    >
    > I wonder why it didn't work with X... This rootfs works with other drivers I've tested (even with the upstream kernel).
    
    Problem found.
    Patch attached.
    
    From: Nelson Castillo <arhuaco@freaks-unidos.net>
    Date: Sat, 24 Oct 2009 14:12:58 -0500
    Subject: [PATCH] Add auto-pst
    
    Without these lines the TS doesn't work.
    I didn't check who the author of the lines is.
    
    Signed-off-by: Nelson Castillo <arhuaco@freaks-unidos.net>

--- arch/arm/mach-s3c2410/include/mach/ts.h
+++ arch/arm/mach-s3c2410/include/mach/ts.h
@@ -16,7 +16,7 @@
 #ifndef __ASM_ARM_TS_H
 #define __ASM_ARM_TS_H
 
-#include <../drivers/input/touchscreen/ts_filter.h>
+#include <linux/input/touchscreen/ts_filter.h>
 
 struct s3c2410_ts_mach_info {
 	/* Touchscreen delay. */

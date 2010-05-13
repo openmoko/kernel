d7cf9cfade693d0b8692de46fac90d9d9b60dad6
Lars-Peter Clausen <lars@metafoo.de>
2010-05-14 00:39:26 +0200

    ASoC: neo1973_wm8753: Move lm4857 specefic code to its own module
    This patch moves the code for the lm4857 AMP from the neo1973_wm8753 sound
    board driver to its own module.
    The lm4857 is a generic AMP IC and not specific to the neo1973.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- sound/soc/codecs/lm4857.c
+++ sound/soc/codecs/lm4857.c
@@ -15,10 +15,10 @@
 
 #include <linux/module.h>
 #include <linux/i2c.h>
+#include <linux/slab.h>
 
 #include <sound/core.h>
 #include <sound/soc.h>
-#include <sound/soc-dapm.h>
 #include <sound/tlv.h>
 
 #include "lm4857.h"
@@ -66,25 +66,26 @@ static int lm4857_write(struct snd_soc_codec *codec, unsigned int reg,
 		unsigned int value)
 {
 	uint8_t data;
+	int ret;
 
 	data = (reg << 6) | value;
 
 	ret = i2c_master_send(codec->control_data, &data, 1);
 	if (ret != 1) {
-		dev_err(&codec->dev, "Failed to write register: %d\n", ret);
+		dev_err(codec->dev, "Failed to write register: %d\n", ret);
 		return ret;
 	}
 
 	return 0;
 }
 
-static unsigned int snd_soc_cache_read(struct snd_soc_codec *codec,
+static unsigned int lm4857_read(struct snd_soc_codec *codec,
 		unsigned int reg)
 {
 	unsigned int val;
 	int ret;
 
-	ret = snd_soc_read_cache(codec, reg, &val);
+	ret = snd_soc_cache_read(codec, reg, &val);
 	if (ret)
 		return -1;
 
@@ -122,6 +123,8 @@ static int lm4857_set_mode(struct snd_kcontrol *kcontrol,
 static int lm4857_set_bias_level(struct snd_soc_codec *codec,
 				 enum snd_soc_bias_level level)
 {
+	struct lm4857 *lm4857 = snd_soc_codec_get_drvdata(codec);
+
 	switch (level) {
 	case SND_SOC_BIAS_ON:
 		snd_soc_update_bits(codec, LM4857_CTRL, 0xF0, lm4857->state);
@@ -130,26 +133,14 @@ static int lm4857_set_bias_level(struct snd_soc_codec *codec,
 		lm4857->state = snd_soc_read(codec, LM4857_CTRL) & 0x0F;
 		snd_soc_update_bits(codec, LM4857_CTRL, 0xF0, 0);
 		break;
+	default:
+		break;
 	}
 
 	codec->dapm.bias_level = level;
 
 	return 0;
 }
-
-static const struct snd_soc_dapm_widget lm4857_widgets[] = {
-    SND_SOC_DAPM_INPUT("RIN"),
-    SND_SOC_DAPM_INPUT("LIN"),
-
-    SND_SOC_DAPM_OUTPUT("LLS");
-    SND_SOC_DAPM_OUTPUT("RLS");
-    SND_SOC_DAPM_OUTPUT("LHP");
-    SND_SOC_DAPM_OUTPUT("RHP");
-    SND_SOC_DAPM_OUTPUT("EP");
-};
-
-static const struct snd_soc_dapm_route audio_map[] = {
-};
 
 static const char *lm4857_mode[] = {
 	"Off",
@@ -174,7 +165,7 @@ static const struct snd_kcontrol_new lm4857_controls[] = {
 	SOC_SINGLE_TLV("Amp Mono Playback Volume", LM4857_MVOL, 0, 31, 0,
 		mono_tlv),
 	SOC_ENUM_EXT("Amp Mode", lm4857_mode_enum[0],
-		lm4857_set_mode, lm4857_get_mode);
+		lm4857_get_mode, lm4857_set_mode),
 	SOC_SINGLE("Amp Spk 3D Playback Switch", LM4857_LVOL, 5, 1, 0),
 	SOC_SINGLE("Amp HP 3D Playback Switch", LM4857_RVOL, 5, 1, 0),
 	SOC_SINGLE("Amp Fast Wakeup Playback Switch", LM4857_CTRL, 5, 1, 0),
@@ -185,20 +176,31 @@ static int lm4857_probe(struct snd_soc_codec *codec)
 {
 	struct lm4857 *lm4857 = snd_soc_codec_get_drvdata(codec);
 
-	codec->control_data = lm4857->i2s;
+	codec->control_data = lm4857->i2c;
 
 	return snd_soc_add_controls(codec, lm4857_controls,
 				ARRAY_SIZE(lm4857_controls));
 }
 
-static int lm4857_remove(struct snd_soc_codec)
-{
-    lm4857_set_bias_level(codec, SND_SOC_BIAS_STANDBY);
-}
-
-static int lm4857_remove(struct snd_soc_codec)
+static int lm4857_remove(struct snd_soc_codec *codec)
 {
     lm4857_set_bias_level(codec, SND_SOC_BIAS_OFF);
+
+    return 0;
+}
+
+static int lm4857_suspend(struct snd_soc_codec *codec, pm_message_t state)
+{
+    lm4857_set_bias_level(codec, SND_SOC_BIAS_OFF);
+
+    return 0;
+}
+
+static int lm4857_resume(struct snd_soc_codec *codec)
+{
+    lm4857_set_bias_level(codec, SND_SOC_BIAS_STANDBY);
+
+    return 0;
 }
 
 static struct snd_soc_codec_driver soc_codec_dev_lm4857 = {
@@ -208,13 +210,13 @@ static struct snd_soc_codec_driver soc_codec_dev_lm4857 = {
 	.remove = lm4857_remove,
 	.suspend = lm4857_suspend,
 	.resume = lm4857_resume,
-	.reg_cache_size = ARRAY_SIZE(lm4857_reg_defaults),
+	.reg_cache_size = ARRAY_SIZE(lm4857_default_regs),
 	.reg_word_size = sizeof(uint8_t),
-	.reg_cache_default = lm4857_reg_defaults,
+	.reg_cache_default = lm4857_default_regs,
 	.set_bias_level = lm4857_set_bias_level,
 };
 
-static int __devinit lm4857_probe(struct i2c_client *client,
+static int __devinit lm4857_i2c_probe(struct i2c_client *i2c,
 	const struct i2c_device_id *id)
 {
 	struct lm4857 *lm4857;
@@ -224,9 +226,9 @@ static int __devinit lm4857_probe(struct i2c_client *client,
 	if (!lm4857)
 		return -ENOMEM;
 
-	i2c_set_clientdata(lm4857);
+	i2c_set_clientdata(i2c, lm4857);
 
-	lm4857->i2c = client;
+	lm4857->i2c = i2c;
 
 	ret = snd_soc_register_codec(&i2c->dev, &soc_codec_dev_lm4857, NULL, 0);
 
@@ -238,11 +240,11 @@ static int __devinit lm4857_probe(struct i2c_client *client,
 	return 0;
 }
 
-static int __devexit lm4857_remove(struct i2c_client *client)
+static int __devexit lm4857_i2c_remove(struct i2c_client *i2c)
 {
-	struct lm4857 *lm4857 = i2c_get_clientdata(client);
+	struct lm4857 *lm4857 = i2c_get_clientdata(i2c);
 
-	snd_soc_unregister_codec(&client->dev);
+	snd_soc_unregister_codec(&i2c->dev);
 	kfree(lm4857);
 
 	return 0;
@@ -256,12 +258,12 @@ MODULE_DEVICE_TABLE(i2c, lm4857_i2c_id);
 
 static struct i2c_driver lm4857_i2c_driver = {
 	.driver = {
-		.name = "LM4857 I2C Amp",
+		.name = "lm4857",
 		.owner = THIS_MODULE,
 	},
-	.probe =          lm4857_probe,
-	.remove =         __devexit_p(lm4857_remove),
-	.id_table =       lm4857_i2c_id,
+	.probe = lm4857_i2c_probe,
+	.remove = __devexit_p(lm4857_i2c_remove),
+	.id_table = lm4857_i2c_id,
 };
 
 static int __init lm4857_init(void)

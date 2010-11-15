69baedf9d18ea86cc3b9916f5eeac9d089d3a117
Arnd Bergmann <arnd@arndb.de>
2010-11-16 00:05:57 +0100

    BKL: mark lock_kernel as deprecated
    No new code should use the big kernel lock, so
    we should really emit compiler warnings to make
    people building out of mainline code aware of
    this.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/smp_lock.h
+++ include/linux/smp_lock.h
@@ -26,10 +26,12 @@ static inline int reacquire_kernel_lock(struct task_struct *task)
 
 extern void __lockfunc
 _lock_kernel(const char *func, const char *file, int line)
+__deprecated
 __acquires(kernel_lock);
 
 extern void __lockfunc
 _unlock_kernel(const char *func, const char *file, int line)
+__deprecated
 __releases(kernel_lock);
 
 #define lock_kernel() do {					\
@@ -40,24 +42,11 @@ __releases(kernel_lock);
 	_unlock_kernel(__func__, __FILE__, __LINE__);		\
 } while (0)
 
-/*
- * Various legacy drivers don't really need the BKL in a specific
- * function, but they *do* need to know that the BKL became available.
- * This function just avoids wrapping a bunch of lock/unlock pairs
- * around code which doesn't really need it.
- */
-static inline void cycle_kernel_lock(void)
-{
-	lock_kernel();
-	unlock_kernel();
-}
-
 #else
 
 #ifdef CONFIG_BKL /* provoke build bug if not set */
 #define lock_kernel()
 #define unlock_kernel()
-#define cycle_kernel_lock()			do { } while(0)
 #define kernel_locked()				1
 #endif /* CONFIG_BKL */
 
